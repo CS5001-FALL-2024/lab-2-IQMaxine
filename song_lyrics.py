2b1e6a2777d8deca69f4fb06968c3ea2624cf5ce
Ikwo Ndiomu <180468465+IQMaxine@users.noreply.github.com>
2024-09-18 14:26:25 -0400

    Update song_lyrics.py

--- song_lyrics.py
+++ song_lyrics.py
@@ -2,8 +2,9 @@
     CS 5001
     Lab 1
     Exercise 2
-    Name:
+    Name: Ikwo Ndiomu
 '''
+
 
 '''
 Choose 10 words of your favorite song lyrics (safe for work only please :)
@@ -12,7 +13,18 @@ lyric on one single line using print.
 '''
 
 def main():
-    # YOUR CODE HERE
+    j = str("Ladies")
+    i = str("and")
+    n = str("gentlemen")
+    x = str("of")
+    y = str("the")
+    o = str("class")
+    u = str("'99")
+    r = str(",")
+    a = str("wear")
+    t = str("sunscreen")
+
+    print(j,i,n,x,y,o,x,u,r,a,t)
 
 
 if __name__ == '__main__':
